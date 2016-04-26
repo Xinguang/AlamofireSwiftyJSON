6ce6e182ada3c99f37318dda2a5aa4ecf48220b3
Xinguang Wang <admin@kansea.com>
2016-04-26 16:00:20 +0900

    swiftlint

--- Source/AlamofireSwiftyJSON.swift
+++ Source/AlamofireSwiftyJSON.swift
@@ -16,57 +16,61 @@ import SwiftyJSON
 extension Request {
     /**
      Adds a handler to be called once the request has finished.
-     
+
      - parameter queue:             The queue on which the completion handler is dispatched.
-     - parameter options:           The JSON serialization reading options. `.AllowFragments` by default.
+     - parameter options:           The JSON serialization reading options.
+                                    `.AllowFragments` by default.
      - parameter completionHandler: A closure to be executed once the request has finished.
-     
+
      - returns: The request.
      */
     public func responseSwiftyJSON(
         queue: dispatch_queue_t? = nil,
         options: NSJSONReadingOptions = .AllowFragments,
         completionHandler: Response<JSON, NSError> -> Void)
-        -> Self
-    {
-        return response(
-            queue: queue,
-            responseSerializer: Request.SwiftyJSONResponseSerializer(options: options),
-            completionHandler: completionHandler
-        )
+        -> Self {
+            return response(
+                queue: queue,
+                responseSerializer: Request.SwiftyJSONResponseSerializer(options: options),
+                completionHandler: completionHandler
+            )
     }
-    
+
     /**
-     Creates a response serializer that returns a SwiftyJSON object constructed from the response data using
-     `NSJSONSerialization` with the specified reading options.
-     
-     - parameter options: The SwiftyJSON serialization reading options. `.AllowFragments` by default.
-     
+     Creates a response serializer that returns a SwiftyJSON object constructed
+     from the response data using `NSJSONSerialization`
+     with the specified reading options.
+
+     - parameter options: The SwiftyJSON serialization reading options.
+                          `.AllowFragments` by default.
+
      - returns: A SwiftyJSON object response serializer.
      */
     public static func SwiftyJSONResponseSerializer(
         options options: NSJSONReadingOptions = .AllowFragments)
-        -> ResponseSerializer<JSON, NSError>
-    {
+        -> ResponseSerializer<JSON, NSError> {
         return ResponseSerializer { _, response, data, error in
             guard error == nil else { return .Failure(error!) }
-            
-            if let response = response where response.statusCode == 204 { return .Success(JSON.null) }
-            
-            guard let validData = data where validData.length > 0 else {
-                let failureReason = "JSON could not be serialized. Input data was nil or zero length."
-                let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
-                return .Failure(error)
+
+            if let response = response where response.statusCode == 204 {
+                return .Success(JSON.null)
             }
-            
+
+            guard let validData = data where validData.length > 0 else {
+                let failureReason = "JSON could not be serialized. " +
+                "Input data was nil or zero length."
+                let e = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
+                return .Failure(e)
+            }
+
             do {
                 let json = try NSJSONSerialization.JSONObjectWithData(validData, options: options)
-                
+
                 return .Success(JSON(json))
             } catch {
                 return .Failure(error as NSError)
             }
         }
     }
-    
+
 }
